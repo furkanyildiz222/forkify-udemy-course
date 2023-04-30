966f493f8ddad91ef9bc5036bbcea404a6e19d8a
furkanyildiz222 <furkan.yildiz222@gmail.com>
2023-04-30 12:21:14 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "8751c9c1-d5cc-49d2-a530-bd5b3cd54617";
+export const MODAL_CLOSE_SEC = 2.5;
